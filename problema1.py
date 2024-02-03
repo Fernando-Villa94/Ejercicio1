700aaffb62299252f9ed59d1a539b7708259bd1d
Fernando-Villa94 <71603388+Fernando-Villa94@users.noreply.github.com>
2024-02-03 15:48:50 -0500

    problema1.py

---
+++ problema1.py
@@ -0,0 +1,15 @@
+
+n=int(input())
+k=2
+r=0
+while k<n:
+  if n%k==0:
+    r=0
+    break
+  else:
+    r=1 
+    pass 
+  k+=1
+
+if r==1:print(f"el valor de n: {n} es primo" )
+else: print(f"el valor de n: {n} no es primo" )
